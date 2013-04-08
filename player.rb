f07e389723ea2fd613e783bb9dc798b84fae8b8c
Steven Barragan <me@steven.mx>
2013-04-07 21:39:46 -0500

    Level 2 passed

--- player.rb
+++ player.rb
@@ -1,9 +1,29 @@
 class Player
+
+  def initialize()
+    @directions = [:forward, :left,  :backward, :right]
+  end
   
   def play_turn(warrior)
-  	
-  	warrior.walk! warrior.direction_of_stairs
+    @warrior = warrior
 
+    enemy = look_enemy
+
+    if enemy
+      warrior.attack! enemy
+    elsif warrior.health < 13
+      warrior.rest!
+    else
+      warrior.walk! warrior.direction_of_stairs
+    end
+
+  end
+
+  def look_enemy()
+    @directions.each{ |direction|
+      return direction if @warrior.feel(direction).enemy?
+    }
+    false
   end
 
 end
